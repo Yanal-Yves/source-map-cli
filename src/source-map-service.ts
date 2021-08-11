dedf51aa66dca3004683983cb51e6dac59baea18
Yanal-Yves FARGIALLA <yanal-yves.fargialla@prothesis.fr>
2021-08-11 17:42:39 +0200

    source-map cli can now find source map over http(s) Improve README.md

--- src/source-map-service.ts
+++ src/source-map-service.ts
@@ -1,13 +1,17 @@
 import chalk from 'chalk';
 import { SourceMapConsumer } from 'source-map';
 import * as fs from 'fs';
+import axios, { AxiosResponse } from 'axios';
 const path = require('path');
 
 export class SourceMapService {
   constructor(private _sourceMapFolderPath: string) {}
 
   public originalPositionFor(generatedFileName: string, lineNumber: number, columnNumber: number): void {
-    const filePath = path.join(this._sourceMapFolderPath, `${generatedFileName}`);
+    const regExp = /^http[s]?:\/\//;
+    const filePath = regExp.test(this._sourceMapFolderPath)
+      ? new URL(`${generatedFileName}`, this._sourceMapFolderPath).href
+      : path.join(this._sourceMapFolderPath, `${generatedFileName}`);
     const sourceMapFilePath = `${filePath}.map`;
     console.log(`According to source map : '${chalk.green(sourceMapFilePath)}'`);
     console.log(
@@ -15,20 +19,47 @@ export class SourceMapService {
         columnNumber
       )} maps to:`
     );
-    fs.readFile(sourceMapFilePath, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
 
-      const whatever = SourceMapConsumer.with(data, null, (consumer) => {
-        console.log(
-          consumer.originalPositionFor({
-            line: lineNumber,
-            column: columnNumber,
-          })
-        );
+    if (regExp.test(this._sourceMapFolderPath)) {
+      this.loadSourceMap(sourceMapFilePath, (res) => {
+        this.originalPositionForInternal(res.data, lineNumber, columnNumber);
       });
+    } else {
+      fs.readFile(sourceMapFilePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        this.originalPositionForInternal(data, lineNumber, columnNumber);
+      });
+    }
+  }
+
+  private loadSourceMap(sourceMapFilePath: string, onfulfilled: (value: AxiosResponse<any>) => void) {
+    axios({
+      method: 'get',
+      url: sourceMapFilePath,
+    })
+      .then((res) => {
+        console.log(res.status);
+        onfulfilled(res);
+      })
+      .catch((err) => {
+        console.log(chalk.red(`Source map not found from ${sourceMapFilePath}`));
+        const log = chalk.red(err);
+        console.log(log);
+      });
+  }
+
+  private originalPositionForInternal(rawSourceMap: string, lineNumber: number, columnNumber: number): void {
+    SourceMapConsumer.with(rawSourceMap, null, (consumer) => {
+      console.log(
+        consumer.originalPositionFor({
+          line: lineNumber,
+          column: columnNumber,
+        })
+      );
     });
   }
 }
